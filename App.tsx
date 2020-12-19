295e088123eef290a2be7a61cea705abf250a6d3
Sülo <gemici.sul@gmail.com>
2020-12-19 21:50:46 +0100

    Only 52 cards are displayed No repetition of the cards Shuffling the cards in the initializationShuffle cards,

--- App.tsx
+++ App.tsx
@@ -1,5 +1,12 @@
 import React from "react";
-import { Button, ColorPropType, StyleSheet, Text, View } from "react-native";
+import {
+  Button,
+  ColorPropType,
+  ImageSourcePropType,
+  StyleSheet,
+  Text,
+  View,
+} from "react-native";
 import { cards } from "./business/cards";
 import { Player, PlayerAction } from "./business/types";
 import { PlayerControls } from "./components/PlayerControls";
@@ -19,22 +26,64 @@ function getPlayer(
 }
 
 const players = [getPlayer("clb", 0, 0), getPlayer("sgm", 1, 0)];
+let shuffleCards = cards;
 export default class App extends React.Component {
   state = {
     game: false,
     activePlayer: players[0],
-    activeCard: this.getRandomCard(),
+    activeCard: this.getFirstCard(),
     PreviousCard: 0,
+    cardIndex: 1,
   };
+
+  resetStates() {
+    this.setState({
+      game: false,
+      activePlayer: players[0],
+      activeCard: this.getFirstCard(),
+      PreviousCard: 0,
+      cardIndex: 1,
+    });
+  }
+  resetStatistics() {
+    var i;
+    for (i = 0; i < players.length; i++) {
+      players[i].statisticDrinkNumber = 0;
+    }
+  }
 
   startGame() {
     //this.setState({ game: !this.state.game })
     this.setState({ game: true });
   }
 
+  shuffle(arra1: Array<ImageSourcePropType>) {
+    var ctr = arra1.length,
+      temp,
+      index;
+    // While there are elements in the array
+    while (ctr > 0) {
+      // Pick a random index
+      index = Math.floor(Math.random() * ctr);
+      // Decrease ctr by 1
+      ctr--;
+      // And swap the last element with it
+      temp = arra1[ctr];
+      arra1[ctr] = arra1[index];
+      arra1[index] = temp;
+    }
+    return arra1;
+  }
+
+  getFirstCard() {
+    shuffleCards = this.shuffle(cards);
+    return cards[0];
+  }
   getRandomCard() {
-    const i = Math.floor(Math.random() * cards.length);
-    return cards[i];
+    // const i = Math.floor(Math.random() * cards.length);
+    // return cards[i];
+    console.log("Cardindex: " + this.state.cardIndex);
+    return shuffleCards[this.state.cardIndex];
   }
 
   nextPlayer() {
@@ -62,7 +111,9 @@ export default class App extends React.Component {
   }
 
   nextCard() {
+    const newCardIndex = this.state.cardIndex + 1;
     this.setState({
+      cardIndex: newCardIndex,
       activeCard: this.getRandomCard(),
     });
     this.nextPlayer();
@@ -70,6 +121,7 @@ export default class App extends React.Component {
 
   onPlayerAction(action: PlayerAction, player: Player) {
     console.log("Start Action");
+
     const message = `Player: ${player.name} pressed action ${action}`;
     console.log(message);
     this.setState({ PreviousCard: this.state.activeCard });
@@ -79,6 +131,11 @@ export default class App extends React.Component {
     if (!this.checkPlayerAction(action)) {
       players[player.index].statisticDrinkNumber =
         players[player.index].statisticDrinkNumber + 1;
+    }
+
+    if (this.state.cardIndex == shuffleCards.length) {
+      this.resetStatistics();
+      this.resetStates();
     }
     console.log("End Action");
     console.log("-------------------");
